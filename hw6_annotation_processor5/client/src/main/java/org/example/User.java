49a830a191bbc892feb2a15a2c79be6b2535512a
Rabbit <dmitrochencko>
2023-05-27 00:07:19 +0300

    created annotation processor

---
+++ hw6_annotation_processor5/client/src/main/java/org/example/User.java
@@ -0,0 +1,14 @@
+package org.example;
+@CustomToString
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
