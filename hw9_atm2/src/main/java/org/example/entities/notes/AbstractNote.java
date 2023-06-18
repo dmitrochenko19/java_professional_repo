8981b89f7506391766dfcf08a1ad7cbf365518e4
Rabbit <dmitrochencko>
2023-06-18 15:25:56 +0300

    completed hw9_atm

---
+++ hw9_atm2/src/main/java/org/example/entities/notes/AbstractNote.java
@@ -0,0 +1,31 @@
+package org.example.entities.notes;
+
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractNote {
+    protected int value;
+
+    /**
+     * @return the smallest number of notes in the sum
+     * equivalent to this note
+     */
+    public abstract List<AbstractNote> change();
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractNote that = (AbstractNote) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ hw9_atm2/src/main/java/org/example/entities/notes/Note500.java
@@ -0,0 +1,14 @@
+package org.example.entities.notes;
+
+import java.util.List;
+
+public class Note500 extends AbstractNote {
+    public Note500() {
+        this.value = 500;
+    }
+
+    @Override
+    public List<AbstractNote> change() {
+        return List.of(new Note100(), new Note100(), new Note100(), new Note100(), new Note100());
+    }
+}
